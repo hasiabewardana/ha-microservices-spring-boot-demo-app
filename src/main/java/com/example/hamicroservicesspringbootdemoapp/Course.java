df7919bf8b1ccfd18db57e365d53d8d2a4c7b575
hasiabewardana <hasi.prasanga@gmail.com>
2022-12-01 22:55:25 +0530

    Created a simple REST API to get course details.

---
+++ src/main/java/com/example/hamicroservicesspringbootdemoapp/Course.java
@@ -0,0 +1,34 @@
+package com.example.hamicroservicesspringbootdemoapp;
+
+public class Course {
+    private int id;
+    private String name;
+    private String institution;
+
+    public Course(int id, String name, String institution) {
+        this.id = id;
+        this.name = name;
+        this.institution = institution;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", institution='" + institution + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/hamicroservicesspringbootdemoapp/CourseController.java
@@ -0,0 +1,21 @@
+package com.example.hamicroservicesspringbootdemoapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Controller class
+@RestController
+public class CourseController {
+    // Controller method
+    @RequestMapping("/course")
+    public List<Course> getAllCourses(){
+        return Arrays.asList(
+                new Course(1, "AWS", "AWS Academy"),
+                new Course(2, "Java", "Java Academy"),
+                new Course(3, "Spring", "Spring Academy")
+        );
+    }
+}
