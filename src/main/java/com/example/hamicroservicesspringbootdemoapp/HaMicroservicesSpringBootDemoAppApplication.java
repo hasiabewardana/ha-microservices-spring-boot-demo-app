991fcc5169765517376db4c4eac309e466fcbcdf
hasiabewardana <hasi.prasanga@gmail.com>
2022-12-01 20:00:09 +0530

    Started the Microservice project.

---
+++ src/main/java/com/example/hamicroservicesspringbootdemoapp/HaMicroservicesSpringBootDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hamicroservicesspringbootdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaMicroservicesSpringBootDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaMicroservicesSpringBootDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hamicroservicesspringbootdemoapp/HaMicroservicesSpringBootDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hamicroservicesspringbootdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaMicroservicesSpringBootDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
