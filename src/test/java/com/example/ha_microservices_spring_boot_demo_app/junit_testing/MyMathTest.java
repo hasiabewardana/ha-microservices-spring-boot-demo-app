24ba1fee71cfef00197c494ee05dcd74d3d9c989
hasiabewardana <hasi.prasanga@gmail.com>
2024-01-09 22:32:49 +0530

    Improved test cases.

--- src/test/java/com/example/ha_microservices_spring_boot_demo_app/junit_testing/MyMathTest.java
+++ src/test/java/com/example/ha_microservices_spring_boot_demo_app/junit_testing/MyMathTest.java
@@ -2,16 +2,19 @@ package com.example.ha_microservices_spring_boot_demo_app.junit_testing;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MyMathTest {
 
+    private final MyMath myMath = new MyMath();
+
     @Test
-    void calculateSum() {
-        MyMath myMath = new MyMath();
-        int[] numbers = {1, 2, 3};
-        int result = myMath.calculateSum(numbers);
-        int expectedResult = 6;
-        assertEquals(expectedResult, result);
+    void calculateSum_ThreeMemberArray() {
+        assertEquals(6, myMath.calculateSum(new int[]{1, 2, 3}));
+    }
+
+    @Test
+    void calculateSum_ZeroLengthArray() {
+        assertEquals(0, myMath.calculateSum(new int[]{}));
     }
 }
