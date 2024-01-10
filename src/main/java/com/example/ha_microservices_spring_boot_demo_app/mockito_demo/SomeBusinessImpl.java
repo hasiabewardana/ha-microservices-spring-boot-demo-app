f7f798da7cbb844ed83d29d53380dc3ba01c8e86
hasiabewardana <hasi.prasanga@gmail.com>
2024-01-10 11:03:34 +0530

    Used stub to write unit test cases.

--- src/main/java/com/example/ha_microservices_spring_boot_demo_app/mockito_demo/SomeBusinessImpl.java
+++ src/main/java/com/example/ha_microservices_spring_boot_demo_app/mockito_demo/SomeBusinessImpl.java
@@ -3,6 +3,10 @@ package com.example.ha_microservices_spring_boot_demo_app.mockito_demo;
 public class SomeBusinessImpl {
     DataService dataService;
 
+    public SomeBusinessImpl(DataService dataService) {
+        this.dataService = dataService;
+    }
+
     public int findTheGreatestFromAllData(){
         int[] data = dataService.retrieveAllData();
         int greatest = Integer.MIN_VALUE;
---
+++ src/test/java/com/example/ha_microservices_spring_boot_demo_app/mockito_demo/SomeBusinessImplTest.java
@@ -0,0 +1,40 @@
+package com.example.ha_microservices_spring_boot_demo_app.mockito_demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SomeBusinessImplTest {
+
+    @Test
+    void findTheGreatestFromAllData_basicScenario() {
+        DataService dataService = new DataServiceStub1();
+        SomeBusinessImpl someBusiness = new SomeBusinessImpl(dataService);
+        int result = someBusiness.findTheGreatestFromAllData();
+        assertEquals(20, result);
+    }
+
+    @Test
+    void findTheGreatestFromAllData_withOneValue() {
+        DataService dataService = new DataServiceStub2();
+        SomeBusinessImpl someBusiness = new SomeBusinessImpl(dataService);
+        int result = someBusiness.findTheGreatestFromAllData();
+        assertEquals(35, result);
+    }
+}
+
+class DataServiceStub1 implements DataService {
+
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{20, 15, 3};
+    }
+}
+
+class DataServiceStub2 implements DataService {
+
+    @Override
+    public int[] retrieveAllData() {
+        return new int[]{35};
+    }
+}
