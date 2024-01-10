22c3c6ce39ab4c375c261358a7ac3d1078bcffa0
hasiabewardana <hasi.prasanga@gmail.com>
2024-01-10 10:31:01 +0530

    Built a sample business impl method for mockito.

---
+++ src/main/java/com/example/ha_microservices_spring_boot_demo_app/mockito_demo/DataService.java
@@ -0,0 +1,5 @@
+package com.example.ha_microservices_spring_boot_demo_app.mockito_demo;
+
+public interface DataService {
+    int[] retrieveAllData();
+}
---
+++ src/main/java/com/example/ha_microservices_spring_boot_demo_app/mockito_demo/SomeBusinessImpl.java
@@ -0,0 +1,17 @@
+package com.example.ha_microservices_spring_boot_demo_app.mockito_demo;
+
+public class SomeBusinessImpl {
+    DataService dataService;
+
+    public int findTheGreatestFromAllData(){
+        int[] data = dataService.retrieveAllData();
+        int greatest = Integer.MIN_VALUE;
+
+        for(int value: data){
+            if(value > greatest)
+                greatest = value;
+        }
+
+        return greatest;
+    }
+}
